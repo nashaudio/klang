c5651e2599b8b50350ddaf0ab1175b34800c8293
Chris Nash <chris.nash@uwe.ac.uk>
2024-10-04 14:41:58 +0100

    - Support for control groups - Group("name", <controls>, ... ) - Fixed Function bugs (first param conflation). - Annoying layout changes by VS.

--- include/plugin.h
+++ include/plugin.h
@@ -69,6 +69,13 @@ extern "C" {                                                                    
         }                                                                                                                   \
         catch (...) { return 1; }                                                                                           \
     }                                                                                                                       \
+                                                                                                                            \
+    VOID_FUNCTION getParameterGroups(void* plugin) {                                                                        \
+        try {                                                                                                               \
+            ((CLASS*)plugin)->groups = klang::Groups;                                                                       \
+        }                                                                                                                   \
+        catch (...) { }                                                                                                     \
+    }                                                                                                                       \
 }
 
 
@@ -196,127 +203,14 @@ namespace MiniPlugin
     using klang::Preset;
     using klang::Presets;
 
-	//template<int SIZE>
- //   struct Text {
- //       char string[SIZE + 1] = { 0 };
-
- //       operator const char* () const { return string; }
- //       const char* c_str() const { return string; }
-
-	//	static Text from(const char* in) {
-	//		Text text;
-	//		text = in;
-	//		return text;
-	//	}
-
- //       void operator=(const char* in) {
- //           memcpy(string, in, SIZE);
- //           string[SIZE] = 0;
- //       }
- //   };
-
- //   typedef Text<32> Caption;
-
- //   struct Parameter
- //   {
- //       enum Type
- //       {
-	//		NONE,	// no control (list terminator)
- //           ROTARY,	// rotary knob (dial/pot)
- //           BUTTON, // push button (trigger)
- //           TOGGLE, // on/off switch (toggle)
- //           SLIDER, // linear slider (fader)
- //           MENU,   // drop-down list (menu; up to 128 items)
- //           METER,  // level meter (read-only: use setParameter() to set value)
- //           WHEEL,  // MIDI control (Pitch Bend / Mod Wheel only)
- //       };
-
-	//	struct Size
- //       {
- //           Size(int x = -1, int y = -1, int width = -1, int height = -1)
- //           : x(x), y(y), width(width), height(height) { }
- //       
- //           int x;
- //           int y;
- //           int width;
- //           int height;
-
- //           bool isAuto() const { return x == -1 && y == -1 && width == -1 && height == -1;  }
- //       };
-
- //       template<typename TYPE, int CAPACITY>
- //       struct Array {
- //           TYPE items[CAPACITY];
- //           unsigned int count = 0;
-
- //           unsigned int size() const { return count; }
-
- //           void add(const TYPE& item) {
- //               if (count < CAPACITY)
- //                   items[count++] = item;
- //           }
-
- //           void clear() {
- //               count = 0;
- //           }
-
- //           TYPE& operator[](int index) { return items[index]; }
- //           const TYPE& operator[](int index) const { return items[index]; }
- //       };
-
- //       typedef Array<Caption, 128> Options;
-
- //       Caption name;           // name for control label / saved parameter
- //       Type type = NONE;       // control type (see above)
- //   
- //       float min;              // minimum control value (e.g. 0.0)
- //       float max;              // maximum control value (e.g. 1.0)
- //       float initial;          // initial value for control (e.g. 0.0)
- //   
- //       Size size;              // position (x,y) and size (height, width) of the control (use AUTO_SIZE for automatic layout)
- //       Options options;        // text options for menus and group buttons
-
- //       float value;            // current control value;
- //       float smoothed;         // smoothed control value;
- //   };
-
 	const Parameter::Size AUTO_SIZE = { -1, -1, -1, -1 };
-	//const Parameter::Options NoOptions;
-
-	//inline static Parameter Dial(const char* name, float min = 0.f, float max = 1.f, float initial = 0.f)
-	//{	return { Caption::from(name), Parameter::ROTARY, min, max, initial, Automatic, NoOptions, initial };	}
-
- //   inline static Parameter Button(const char* name)
-	//{	return { Caption::from(name), Parameter::BUTTON, 0, 1, 0.f, Automatic, NoOptions, 0.f };	}
-
- //   inline static Parameter Toggle(const char* name, bool initial = false)
-	//{	return { Caption::from(name), Parameter::TOGGLE, 0, 1, initial ? 1.f : 0.f, Automatic, NoOptions, initial ? 1.f : 0.f};	}
-
- //   inline static Parameter Slider(const char* name, float min = 0.f, float max = 1.f, float initial = 0.f)
-	//{	return { Caption::from(name), Parameter::SLIDER, min, max, initial, Automatic, NoOptions, initial }; }
-
-	//template<typename... Options>
- //   inline static Parameter Menu(const char* name, const Options... options)
-	//{	Parameter::Options menu;
-	//    const char* strings[] = { options... };
- //       int nbValues = sizeof...(options);
- //       for(int p=0; p<nbValues; p++)
- //           menu.add(Caption::from(strings[p]));
-	//	return { Caption::from(name), Parameter::MENU, 0, menu.size() - 1.f, 0, Automatic, menu, 0 }; 
-	//}
-
- //   inline static Parameter Meter(const char* name, float min = 0.f, float max = 1.f, float initial = 0.f)
-	//{	return { Caption::from(name), Parameter::METER, min, max, initial, Automatic, NoOptions, initial }; }
-
- //   inline static Parameter PitchBend()
-	//{	return { { "PITCH\nBEND" }, Parameter::WHEEL, 0.f, 16384.f, 8192.f, Automatic, NoOptions, 8192.f }; }
-
- //   inline static Parameter ModWheel()
-	//{	return { { "MOD\nWHEEL" }, Parameter::WHEEL, 0.f, 127.f, 0.f, Automatic, NoOptions, 0.f }; }
-
+	
     struct Parameters : Array<Parameter, 128>
     {
         float value[128] = { 0 };
+
+        typedef klang::Control::Group Group;
+        typedef Array<Group, 32> Groups;
 
         void operator+= (const Parameter& control) {
             items[count++] = control;
@@ -372,63 +266,7 @@ namespace MiniPlugin
             }
             return changed;
         }
-
-        //float& operator[](int index) { return items[index].value; }
-        //signal operator[](int index) const { return items[index].value; }
-
-        //Control& operator()(int index) { return items[index]; }
-        //Control operator()(int index) const { return items[index]; }
     };
-
-  //  struct Parameters : Array<Parameter, 128>
-  //  {
-		//void operator+= (const Parameter& control) {
-		//	items[count++] = control;
-		//}
-
-		//void operator= (const Parameters& controls) {
-		//	for (int c = 0; c < 128 && controls(c).type != Parameter::NONE; c++)
-		//		operator+=(controls(c));
-		//}
-
-  //      void add(const char* name, Parameter::Type type = Parameter::ROTARY, float min = 0.f, float max = 1.f, float initial = 0.f, Parameter::Size size = AUTO_SIZE) {
-  //          items[count].name = name;
-  //          items[count].type = type;
-  //          items[count].min = min;
-  //          items[count].max = max;
-  //          items[count].initial = initial;
-  //          items[count].size = size;
-  //          items[count++].value = initial;
-  //      }
-
-  //      float& operator[](int index) { return items[index].value; }
-  //      float operator[](int index) const { return items[index].value; }
-
-  //      Parameter& operator()(int index) { return items[index]; }
-  //      Parameter operator()(int index) const { return items[index]; }
-  //  };
-
- //   struct Preset {
- //       Caption name;
- //       Array<float, 128> values;
- //   };
-
-	//struct Presets : Array<Preset, 128> {
-	//	void operator += (const Preset& preset) {
-	//		items[count++] = preset;
-	//	}
-
- //       template<typename... Values>
- //       void add(const char* name, const Values... values) {
- //           items[count].name = name;
-
- //           const float preset[] = { values... };
- //           int nbValues = sizeof...(values);
- //           for(int p=0; p<nbValues; p++)
- //               items[count].values.add(preset[p]);
- //           count++;
- //       }
- //   };
 
     struct Debug {
         const float* buffer = nullptr;
@@ -441,10 +279,7 @@ namespace MiniPlugin
 
     class Effect
     {
-    public:
-        //Effect() { }
-        //virtual ~Effect() { }
-        
+    public:       
         virtual void process(const float** inputBuffers, float** outputBuffers, int numSamples) = 0;
         
         // events
@@ -476,6 +311,7 @@ namespace MiniPlugin
         }
         
         Parameters parameters;
+        Parameters::Groups groups;
         Presets presets;
 
         Debug debug;
